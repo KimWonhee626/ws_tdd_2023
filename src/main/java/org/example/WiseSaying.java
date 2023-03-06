2aa38030e68a79482c6625ddd6128b6b60306a46
KimWonhee626 <69297345+KimWonhee626@users.noreply.github.com>
2023-03-06 17:45:23 +0900

    목록 출력 기능 추가 (#6)

---
+++ src/main/java/org/example/WiseSaying.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class WiseSaying {
+
+    public long id;
+    public String content;
+    public String author;
+
+    public WiseSaying(long id, String content, String author) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
+    }
+}
